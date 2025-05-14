599cbaaf493848dc46cf1c546462b4ff23acb1ba
boarnerges <olujawo1996@gmail.com>
2025-05-14 15:48:49 +0100

    name resoltion

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import About from "./componenets/about/About";
 
 import Portfolio from "./componenets/portfolio/Portfolio";
 
-import NewHeader from "./componenets/newheader/newheader";
+import NewHeader from "./componenets/newheader/NewHeader";
 import Cta from "./componenets/CTA/cta";
 
 const App = () => {
